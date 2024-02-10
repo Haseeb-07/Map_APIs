b4d9672dbbc7629acdb42019b18838bc2ea7b28b
Haseeb-07 <haseebalichaudhary3@gmail.com>
2024-02-10 10:01:02 +0500

    Maps APIs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import Map from './Map';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Map App</h1>
+      </header>
+      <main>
+        <Map />
+      </main>
+    </div>
+  );
+}
+
+export default App;
